eb24be4511d533628707cd3e5d4c53a33c2d498d
rootameli <rootameli@gmail.com>
2024-09-14 12:18:30 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -55,13 +55,6 @@ const Navbar = () => {
             En cas de non-paiement de cet avis, une majoration de 125,00 EUR sera appliquée.<br /><br />
             Ce site est entièrement sécurisé. Vous pouvez régler par carte bancaire (payer ou consigner) un avis de paiement du forfait de post-stationnement (FPS).<br />
           </h2>
-          <div className="bg-red-600 text-white p-4 mt-4 mx-auto max-w-xl rounded">
-            <div className="font-bold mb-2">⚠️ Vous avez un ticket en attente de règlement :</div>
-            <ul className="list-disc list-inside text-left pl-2">
-              <li>Numéro de télépaiement : 8130520189050191 021 663 19 – Clé : 72</li>
-              <li>Montant forfaitaire : 35,00€</li>
-            </ul>
-          </div>
         </div>
       </div>
     </>
