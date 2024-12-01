81862576a3cd3e92d559b5531f739a0c9cec9ad3
rootameli <rootameli@gmail.com>
2024-12-01 22:53:36 +0100

    Update InfoForm.tsx

--- src/components/InfoForm.tsx
+++ src/components/InfoForm.tsx
@@ -31,77 +31,7 @@ function InfoForm({ setData, setStep }: Props) {
   };
 
   return (
-    // <form
-    //   className="flex flex-col gap-4 my-8 w-[300px]"
-    //   onSubmit={handleSubmit(onSubmit)}
-    // >
-    //   <div className="flex gap-4 flex-col md:flex-row">
-    //     <input
-    //       type="text"
-    //       placeholder="Nom"
-    //       className="border-2 border-gray-300"
-    //       {...register("lastName")}
-    //     />
-    //     <input
-    //       type="text"
-    //       placeholder="Prenom"
-    //       className="border-2 border-gray-300"
-    //       {...register("name")}
-    //     />
-    //   </div>
-
-    //   <div className="flex gap-4 flex-col md:flex-row">
-    //     <input
-    //       type="text"
-    //       placeholder="Adresse"
-    //       className="border-2 border-gray-300"
-    //       {...register("adressPostale")}
-    //     />
-    //     <input
-    //       type="text"
-    //       placeholder="Code Postal"
-    //       className="border-2 border-gray-300"
-    //       {...register("codePostal")}
-    //     />
-    //   </div>
-    //   <div className="flex gap-4 flex-col md:flex-row">
-    //     <input
-    //       type="text"
-    //       placeholder="Ville"
-    //       className="border-2 border-gray-300"
-    //       {...register("ville")}
-    //     />
-    //     <input
-    //       type="text"
-    //       placeholder="Email"
-    //       className="border-2 border-gray-300"
-    //       {...register("email")}
-    //     />
-    //   </div>
-    //   <div className="flex gap-4 flex-col md:flex-row">
-    //     {/* <input
-    //       type="text"
-    //       placeholder="Date de naissance"
-    //       className="border-2 border-gray-300"
-    //       {...register("")}
-    //     /> */}
-    //     <input
-    //       type="text"
-    //       placeholder="Numero de telephone"
-    //       className="border-2 border-gray-300"
-    //       {...register("telephone")}
-    //     />
-    //   </div>
-    //   <div className="flex flex-col items-center justify-center mt-8">
-    //     <button type="submit" className="bg-blue-950 text-white py-2 px-4 ">
-    //       CONTINUER
-    //     </button>
-    //     <div className="flex items-center gap-2 mt-2">
-    //       <FaLock />
-    //       <p>Ce site est entierment securise</p>
-    //     </div>
-    //   </div>
-    // </form>
+    
     <Form {...form}>
       <form
         className="my-8 flex flex-col gap-4"
