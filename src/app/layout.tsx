c4bf994973e075a6514e6fa50914dd04d9e7778a
rootameli <rootameli@gmail.com>
2024-12-04 10:57:23 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Amende",
+  title: "Telepaiement FPS",
 };
 
 export default function RootLayout({
