8bf5e704e51ad14bcf0629796469353ea2d804e3
rootameli <rootameli@gmail.com>
2024-12-24 14:16:41 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Service-FPS",
+  title: "Espace Tele-paiement",
 };
 
 export default function RootLayout({
